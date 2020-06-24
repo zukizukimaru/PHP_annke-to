7e7f14bb2c0f4401d7d8ece2dd1ec43dbc6f6fb7
zukizukimaru <kazucchi.98323@gmail.com>
2020-06-24 17:12:48 +0900

    サンクスページを導入した

---
+++ check.php
@@ -0,0 +1,57 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charaset=UTF-8">
+    <title>PHP基礎</title>
+  </head>
+
+  <body>
+
+ <?php
+$nickname=$_POST['nickname'];
+$email=$_POST['email'];
+$goiken=$_POST['goiken'];
+
+if($nickname == '') {
+
+  print'ニックネームが入力されてません。<br/>';
+
+}  else {
+  print'ようこそ';
+  print $_POST['nickname'];
+  print'様';
+  print'<br/>';
+}
+
+if($email=='')
+{
+  print'メールアドレスが入力されていません。<br/>';
+}
+else {
+  print 'メールアドレス:';
+  print$email;
+  print'<br/>';
+}
+
+if($goiken=='') {
+ print'ご意見が入力されていません。<br/>';
+
+} 
+else{
+  print'ご意見『';
+  print$goiken;
+  print'』<br/>';
+}
+
+print'<form method="post" action="thanks.php">';
+print'<input type="button" onclick="history.back()" value="戻る">';
+print'<input type="submit" value="OK">';
+print'</form>';
+
+?>
+
+  </body>
+</html>
+
+
+
---
+++ thanks.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charaset=UTF-8">
+    <title>PHP基礎</title>
+  </head>
+  <body>
+    <?php
+    print'ご意見ありがとうございました</br>';
+    ?>
+  </body>
+</html>
